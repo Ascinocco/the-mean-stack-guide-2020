47d8b3444fc925045239dbe2cc8595032af7fd7e
Anthony Scinocco <scinocco.a@gmail.com>
2020-10-03 19:21:42 -0400

    updated app middleware configuration to allow cors for any client, addtional http verbs and headers

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  // allow and client to access resources
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // allow these non default headers if the request contains them
+  res.setHeader(
+    'Access-Control-Allow-Header',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  // allow these http methods
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  )
+  next();
+});
+
 app.use('/api/posts', (req, res, next) => {
   const posts = [
     { id: 'woiegjiew', title: 'Post 1', content: 'some content 1' },
