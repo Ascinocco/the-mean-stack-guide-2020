c1cfa50bc7a9f1a1edd62049c337eecc98f27512
Anthony Scinocco <scinocco.a@gmail.com>
2020-10-04 19:16:23 -0400

    forms for login and sign up created

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  templateUrl: './login.component.html',
+  templateUrl: './signup.component.html',
   styleUrls: [
-    './login.component.css',
+    './signup.component.css',
   ],
 })
 
-export class LoginComponent {
+export class SignupComponent {
   isLoading = false;
 
-  onLogin(form: NgForm) {
+  onSignup(form: NgForm) {
     console.log('form-value', form.value)
   }
 }
