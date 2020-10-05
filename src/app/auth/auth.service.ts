75e5ab162164a991f15fb8522269eea87b406572
Anthony Scinocco <scinocco.a@gmail.com>
2020-10-05 17:06:27 -0400

    setup auto auth flow

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -40,7 +40,25 @@ export class AuthService {
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
     clearTimeout(this.tokenTimer);
+    this.clearAuthData();
     this.router.navigate(['/']);
+  }
+
+  autoAuthUser() {
+    const authData = this.getAuthData();
+
+    if (!authData) {
+      return;
+    }
+
+    const now = new Date();
+    const expiresIn = authData.expirationDate.getTime() - now.getTime();
+    if (expiresIn > 0) {
+      this.token = authData.token;
+      this.isAuthenticated = true;
+      this.setAuthTimer(expiresIn / 1000);
+      this.authStatusListener.next(true);
+    }
   }
 
   login(email: string, password: string) {
@@ -51,14 +69,44 @@ export class AuthService {
         this.token = data.token;
 
         if (this.token) {
-          this.tokenTimer = setTimeout(() => {
-            // better ux might be to have an alert to avoid logging out mid-action
-            this.logout();
-          }, data.expiresIn * 1000);
+          this.setAuthTimer(data.expiresIn);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
+          const expirationDate = new Date(new Date().getTime() + (data.expiresIn * 1000));
+          this.saveAuthData(this.token, expirationDate);
           this.router.navigate(['/']);
         }
       });
+  }
+
+  private saveAuthData(token: string, expirationDate: Date) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expirationDate', expirationDate.toISOString());
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('token');
+
+    if (!token || !expirationDate) {
+      return;
+    }
+
+    return {
+      token,
+      expirationDate: new Date(expirationDate),
+    };
+  }
+
+  private setAuthTimer(duration: number) {
+    this.tokenTimer = setTimeout(() => {
+      // better ux might be to have an alert to avoid logging out mid-action
+      this.logout();
+    }, duration * 1000);
   }
 }
