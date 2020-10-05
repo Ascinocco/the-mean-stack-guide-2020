9bcb4535e830ed36b19c7a9e7888278cbaa81188
Anthony Scinocco <scinocco.a@gmail.com>
2020-10-05 16:05:41 -0400

    improve login logout ux

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) {}
 
@@ -22,12 +24,21 @@ export class AuthService {
     return this.token;
   }
 
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
+
+  logout() {
+
+  }
+
   login(email: string, password: string) {
     const authData: AuthData = { email, password };
     console.log('authData', authData);
     this.http.post<{ token: string; }>('http://localhost:3000/api/user/login', authData)
       .subscribe((data) => {
         this.token = data.token;
+        this.authStatusListener.next(true);
       });
   }
 }
